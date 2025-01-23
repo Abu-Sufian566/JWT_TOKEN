654533f25f715e84d9e17cf29f59d2df742c6bb8
abu.sufian6098@gmail.com <abu.sufian6098@gmail.com>
2025-01-24 04:00:43 +0500

    Add_JWTTOKEN

---
+++ JWT_TOKEN/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWT_TOKEN.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string? UserName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string? Password { get; set; }
+    }
+}
